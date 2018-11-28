3641d2c9b5ef68f1e4d1b2532b8db97b1b2b7f3b
Suthep Chanchuphol <moo_suthep@hotmail.com>
2018-11-28 20:40:41 +0700

    Typos, KaTeX
    ^ Fix typos
    + Use KaTeX to render maths
    ^ Fix mobile margin.
    ^ Fix line height and letter spacing

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 (function(){
+  // Disable middle click scroller
   document.body.onmousedown = function (e) { if (e.button === 1) return false; }
+
+  // render maths
+  var maths = [...document.getElementsByClassName("math")];
+  maths.forEach(e => {
+    katex.render(e.innerText, e, {
+      throwOnError: false
+    });
+  })
 }());
